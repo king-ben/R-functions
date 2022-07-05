07c8b9fbe27687b44de188b24dbb8a10a9adf3b1
Ben King <57047255+king-ben@users.noreply.github.com>
2022-07-05 10:54:02 +0200

    Update tip.height.R

--- tip.height.R
+++ tip.height.R
@@ -2,3 +2,11 @@
 #requires phytools library
 tip.height <- function(tree, tip){return(max(nodeHeights(tree))-nodeheight(tree, which(tree$tip.label == tip)))}
 
+tipHeights <- function(tree){
+  max <- max(nodeHeights(tree))
+  th <- vector()
+  for(i in 1:length(tree$tip.label)){
+    th[i] <- nodeheight(tree, i)
+  }
+  return(max-th)
+}
