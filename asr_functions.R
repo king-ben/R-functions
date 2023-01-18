7a09a5cb99e3837821a2e8f7a43c448625a10a41
Ben King <benking315@gmail.com>
2023-01-18 10:34:58 +0100

    update asr function with beast2.7 class names

--- asr_functions.R
+++ asr_functions.R
@@ -177,12 +177,12 @@ asr_blocks <- function(parts, names=NULL, links, id="AncestralSequenceLogger", l
     link <- links[which(links[,1]==pnam),2]
     first <- parts[i, 2]
     last <- parts[i, 3]
-    xmlAttrs(log) <- c(id=paste(id, pnam, sep="."), spec="beast.evolution.likelihood.AncestralStateLogger", data=paste("@orgdata", pnam, sep="."), siteModel=paste("@SiteModel.s:", link, sep=""), branchRateModel="@RelaxedClock.c:clock", tree="@Tree.t:tree", taxonset=paste("@", taxonset, sep=""))
+    xmlAttrs(log) <- c(id=paste(id, pnam, sep="."), spec="beastlabs.evolution.likelihood.AncestralStateLogger", data=paste("@orgdata", pnam, sep="."), siteModel=paste("@SiteModel.s:", link, sep=""), branchRateModel="@RelaxedClock.c:clock", tree="@Tree.t:tree", taxonset=paste("@", taxonset, sep=""))
     if(is.null(names)){
       xmlAttrs(log) <- c(value=paste(paste0(pnam, ".ascertainment"), paste0(pnam, ".", 1:(last-first), collapse=" ")))
     } else{
       cnam <- names[first:last]
-      xmlAttrs(log) <- c(id=paste(id, pnam, sep="."), spec="beast.evolution.likelihood.AncestralStateLogger", data=paste("@orgdata", pnam, sep="."), siteModel=paste("@SiteModel.s:", link, sep=""), branchRateModel="@RelaxedClock.c:clock", tree="@Tree.t:tree", value=paste(cnam, collapse=" "), taxonset=paste("@", taxonset, sep=""))
+      xmlAttrs(log) <- c(id=paste(id, pnam, sep="."), spec="beastlabs.evolution.likelihood.AncestralStateLogger", data=paste("@orgdata", pnam, sep="."), siteModel=paste("@SiteModel.s:", link, sep=""), branchRateModel="@RelaxedClock.c:clock", tree="@Tree.t:tree", value=paste(cnam, collapse=" "), taxonset=paste("@", taxonset, sep=""))
     }
     if(logOrigin==TRUE){
       xmlAttrs(log) <- c(logParent="true", logMRCA="false")
